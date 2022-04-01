63a23a3e66733c5abb82f488f70c27009220ee4e
Harry <MingChenXX@163.com>
2022-04-01 10:43:19 +0800

    change: 修改tooltip的demo

--- src/site/tooltip/positonTooltip.tsx
+++ src/site/tooltip/positonTooltip.tsx
@@ -2,87 +2,51 @@ import { Tooltip } from '../../index'
 import React from 'react'
 import './index.less'
 import '../../component/tooltip/style'
+import { TooltipPlacement } from '../../component/tooltip'
 
 export default () => {
+  function createTooltip(title: string, placement: TooltipPlacement) {
+    return (
+      <Tooltip title="提示文字样式" placement={placement}>
+        <span className="span">{title}</span>
+      </Tooltip>
+    )
+  }
   return (
     <div className="tooltip-box">
       <div className="row1">
-        <div className="data1">
-          <Tooltip title="提示文字" placement="topLeft">
-            <span className="span">上左</span>
-          </Tooltip>
-        </div>
+        <div className="content">
+          <div className="data1">{createTooltip('上左', 'topLeft')}</div>
 
-        <div className="data1">
-          <Tooltip title="提示文字" placement="top">
-            <span className="span">上中</span>
-          </Tooltip>
-        </div>
+          <div className="data1">{createTooltip('上中', 'top')}</div>
 
-        <div className="data1">
-          <Tooltip title="提示文字" placement="topRight">
-            <span className="span">上右</span>
-          </Tooltip>
+          <div className="data1">{createTooltip('上右', 'topRight')}</div>
         </div>
       </div>
 
       <div className="column1">
-        <div className="data1">
-          <Tooltip title="左上提示文字样式" placement="leftTop">
-            <span className="span">左上(鼠标移入)</span>
-          </Tooltip>
-        </div>
+        <div className="data1">{createTooltip('左上(鼠标移入)', 'leftTop')}</div>
 
-        <div className="data1">
-          <Tooltip title="左中提示文字样式" placement="left">
-            <span className="span">左中(鼠标移入)</span>
-          </Tooltip>
-        </div>
+        <div className="data1">{createTooltip('左中(鼠标移入)', 'left')}</div>
 
-        <div className="data1">
-          <Tooltip title="左下提示文字样式" placement="leftBottom">
-            <span className="span">左下(鼠标移入)</span>
-          </Tooltip>
-        </div>
+        <div className="data1">{createTooltip('左下(鼠标移入)', 'leftBottom')}</div>
       </div>
 
       <div className="column2">
-        <div className="data1">
-          <Tooltip title="右上提示文字样式" placement="rightTop">
-            <span className="span">右上(鼠标移入)</span>
-          </Tooltip>
-        </div>
+        <div className="data1">{createTooltip('右上(鼠标移入)', 'rightTop')}</div>
 
-        <div className="data1">
-          <Tooltip title="右中提示文字样式" placement="right">
-            <span className="span">右中(鼠标移入)</span>
-          </Tooltip>
-        </div>
+        <div className="data1">{createTooltip('右中(鼠标移入)', 'right')}</div>
 
-        <div className="data1">
-          <Tooltip title="右下提示文字样式" placement="rightBottom">
-            <span className="span">右下(鼠标移入)</span>
-          </Tooltip>
-        </div>
+        <div className="data1">{createTooltip('右下(鼠标移入)', 'rightBottom')}</div>
       </div>
 
       <div className="row2">
-        <div className="data1">
-          <Tooltip title="文字样式" placement="bottomLeft">
-            <span className="span">下左</span>
-          </Tooltip>
-        </div>
+        <div className="content">
+          <div className="data1">{createTooltip('下左', 'bottomLeft')}</div>
 
-        <div className="data1">
-          <Tooltip title="文字样式" placement="bottom">
-            <span className="span">下中</span>
-          </Tooltip>
-        </div>
+          <div className="data1">{createTooltip('下中', 'bottom')}</div>
 
-        <div className="data1">
-          <Tooltip title="文字样式" placement="bottomRight">
-            <span className="span">下右</span>
-          </Tooltip>
+          <div className="data1">{createTooltip('下右', 'bottomRight')}</div>
         </div>
       </div>
     </div>
